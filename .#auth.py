7eea861c690353bd156e679a7d29888433e0866a
Timothy Ong <Timothy@timothys-mbp.home>
2013-11-28 16:22:38 -0500

    finished app.py

---
+++ .#auth.py
@@ -0,0 +1,1 @@
+Timothy@timothys-mbp.home.14616
--- app.py
+++ app.py
@@ -1,49 +1,89 @@
 from flask import Flask, render_template, url_for, redirect, request, session
-import utils
+import utils, auth
 
 app = Flask(__name__)
 app.secret_key = 'secretkey'
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    if 'username' in session:
+        return render_template('home.html', username=session['username'])
+    else:
+        return render_template('home.html')
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method=="GET":
-        return render_template('login.html')
+        if 'username' in session:
+            return render_template("login.html",username = session["username"])
+        else:
+            return render_template("login.html")
     else:
         username = request.form["username"].encode("ascii","ignore")
         password = request.form["password"].encode("ascii","ignore")
-        if (utils.authenticate(username,password)):
+        if (auth.login(username,password)):
             session["username"] = username
-            return redirect(url_for("portfolio"))
+            return render_template('portfolio.html', username=session['username'])
         else:
-            return redirect(url_for("register"))
+            return redirect(url_for("login"))
 
-            @app.route("/register", methods=["GET", "POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
-        return render_template("register.html")
+        if 'username' in session:
+            return render_template("redirect.html",username = session["username"])
+        else:
+            return render_template("redirect.html")
     else:
         username = request.form["username"].encode("ascii","ignore")
         password = request.form["password"].encode("ascii","ignore")
         if (auth.register(username,password)):
             session["username"] = username
-            return redirect(url_for("login")) #login after registering or log in automatically?
+            return render_template('portfolio.html', username=session['username'])
         else:
             return redirect(url_for("register")) #return specific error?
 
 @app.route("/portfolio")
 def portfolio():
-    if logged_in():
-        
+    if 'username' in session:
+        user = session['username']
+        return render_template("portfolio.html",username = user,cash=auth.getCash(user), stocks = auth.getStocks(user))
+    else:
+        return render_template("portfolio.html")
+    
+@app.route("/buynsell")
+def buynsell():
+    if request.method == "GET":
+        if 'username' in session:
+            return render_template("buynsell.html", username = session['username'])
+        else:
+            return render_template('buynsell.html')
+    else:
+        symb = request.form['symbol']
+        num = request.form['number']
+        action = request.form['action']
+        user = session['username']
+        if action == 'buy':
+            auth.buy(session['username'], symb, num)
+            return render_template('portfolio.html', username = user, cash=auth.getCash(user), stocks = auth.getStocks(user))
+        else:
+            auth.sell(session['username'], symb, num)
+            return render_template('portfolio.html', username = user, cash=auth.getCash(user), stocks = auth.getStocks(user))
 
 @app.route("/search", methods=["GET", "POST"])
 def search():
-
-@app.route("/stocks")
-def stocks():
+    if request.method == "GET":
+        if 'username' in session:
+            return render_template("search.html",username = session["username"])
+        else:
+            return render_template("search.html")
+    else:
+        symb = request.form['symb']
+        q = utils.init(symb)
+        if 'username' in sesssion:
+            return render_template("stocks.html",username = session["username"], q=q)
+        else:
+            return render_template("stocks.html", q=q)
 
 @app.route("/logout")
 def logout():
